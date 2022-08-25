38dc9375a5a3ca138e207a747feba1a663601dd2
abdullahyousaf13 <90313841+AbdullahYousaf-13@users.noreply.github.com>
2022-08-25 11:45:14 +0500

    Done till Functions as Parameters

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Flutter Basics:
+
 A repository covering Flutter basics
 
 - - -
@@ -1249,102 +1250,7 @@ or
 
 #### Functions as Parameter Code:
 
-##### main.dart:
 
-    import 'package:flutter/material.dart';
-    import 'quote.dart';
-    import 'quote_card.dart';
-
-    void main() => runApp(MaterialApp(
-      home: QouteList(),
-    ));
-
-    class QouteList extends StatefulWidget {
-      const QouteList({Key? key}) : super(key: key);
-
-      @override
-      State<QouteList> createState() => _QouteListState();
-    }
-
-    class _QouteListState extends State<QouteList> {
-
-      List<Quote> quotes = [
-        Quote(author: 'Osca Wilde', text: 'I have nothing to declare except my genius'),
-        Quote(author: 'Osca Wilde', text: 'The truth is rarely pure and never simple'),
-        Quote(author: 'Osca Wilde', text: 'Be yourself; everybody is already taken')
-      ];
-
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          backgroundColor: Colors.grey[900],
-          appBar: AppBar(
-            title: Text('Awesome Quotes'),
-            centerTitle: true,
-            backgroundColor: Colors.black,
-          ),
-          body: Column (
-            children: quotes.map((quote) => QuoteCard(
-              quote: quote,
-              delete: () {
-                setState(() {
-                  quotes.remove(quote);
-                });
-              }
-            )).toList(),
-          ),
-        );
-      }
-    }
-
-##### quote_card.dart:
-
-    import 'package:flutter/material.dart';
-    import 'quote.dart';
-
-    class QuoteCard extends StatelessWidget {
-
-      final Quote quote;
-      final Function delete;
-      QuoteCard ({required this.quote, required this.delete});
-
-      @override
-      Widget build(BuildContext context) {
-        return Card(
-          color: Colors.blueGrey,
-          margin: EdgeInsets.all(16),
-          child: Padding(
-            padding: EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget> [
-                Text(
-                  quote.text,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Text(
-                  quote.author,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.grey[800],
-                  ),
-                ),
-                SizedBox(height: 8),
-                FlatButton.icon(
-                  onPressed: () => delete(),
-                  label: Text('Delete Quote'),
-                  icon: Icon(Icons.delete),
-                ),
-              ],
-            ),
-          ),
-        );
-      }
-    }
 
 - - -
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/pages/home.dart';
+//import 'pages/home.dart';
+//import 'pages/choose_location.dart';
+//import 'pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyCard(),
+  home: Home(),
 ));
 
-class MyCard extends StatefulWidget {
-  const MyCard({Key? key}) : super(key: key);
-  @override
-  State<MyCard> createState() => _MyCardState();
-}
 
-class _MyCardState extends State<MyCard> {
 
-  int ninjaLevel = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('My ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget> [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/Dark-Knight.png'),
-                radius: 40,
 
-              ),
-            ),
-            Divider(
-              height: 90,
-              color: Colors.grey,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Dark-Knight',
-              style: TextStyle(
-                color: Colors.blueGrey,
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.blueGrey,
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'CONTACT',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.blueGrey,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'abdullahyousaf132@gmail.com',
-                  style: TextStyle(
-                    color: Colors.blueGrey,
-                    letterSpacing: 1,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
